a7c9f7a4546c693be33254183539292285419b10
tlariviere <tlariviere@enseirb-matmeca.fr>
2017-01-28 11:23:31 +0100

    Parameters.hpp

--- Parameters.hpp
+++ Parameters.hpp
@@ -7,11 +7,12 @@ const int nx=40, ny=40;
 const double lx=1., ly=1., dt=10.;
 const double dx=lx/(nx+1), dy=ly/(ny+1);
 const int nl = nx*ny;
-const int nIterMax = 1000;
+const int nIterMax = 10000;
 const double pi = 3.1415926535897932384;
 const int p=1;
 const int D = 1;
 const int BC = 1;
+const int SOLVER = 0;
 const double a = 1.0;
 const double b = 1.0;
 
